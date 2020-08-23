6701235922745df0a9fe15e9d16c1bfdd0c7bf4d
Methkal <methkal.khalawi@gmail.com>
2020-08-23 02:42:16 +0200

    add morgan middleware package to the dev environment

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,7 +2,7 @@
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "show all bootcamps" });
+  res.status(200).json({ success: true, msg: "show all bootcamps"});
 };
 
 // @desc get signle bootcamps
--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const bootcamps = require("./routes/bootcamps");
 
+const morgan = require("morgan");
+
 // load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use("/api/v1/bootcamps", bootcamps);
 
