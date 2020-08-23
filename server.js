2e28b1fad30c1f118775984db915114e15c762ed
Methkal <methkal.khalawi@gmail.com>
2020-08-23 17:33:43 +0200

    add connect to mongodb configs

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 const morgan = require("morgan");
 
 // load env vars
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
 
 const app = express();
 
@@ -17,7 +20,13 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
 );
+
+// handle unhandld promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
